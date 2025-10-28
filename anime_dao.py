cab43c5b5653a0e9843114420ea24e90ae533423
albertodetdev <delgadotoralalberto@gmail.com>
2025-10-28 13:18:07 +0100

    Recomendaciones por consola y version, testear entrenamiento por la aplicacion

--- anime_dao.py
+++ anime_dao.py
@@ -1,22 +1,25 @@
 from anime_recomendator import entrenar
 from user_recomendator import recomendar_animes
+from pathlib import Path
 
 class AnimeDAO:
     def __init__(self):
-        self.version_file = "data/version.txt"
+        self.version_file = Path("data/version.txt")
 
-    def get_version(self) -> str:
+    def get_version(self):
         try:
-            with open(self.version_file, "r") as f:
-                version = f.read().strip()
-            return version
+            with self.version_file.open("r", encoding="utf-8") as f:
+                contenido = f.read().strip()
+                return int(contenido) if contenido.isdigit() else 0
         except FileNotFoundError:
-            return "Versión no encontrada"
+            self.version_file.parent.mkdir(parents=True, exist_ok=True)
+            self.version_file.write_text("0", encoding="utf-8")
+            return 0
 
-    def train(self) -> str:
+    def train(self):
         entrenar()
-        return "Entrenamiento completado"
 
-    def get_recommendations(self, user_id: int) -> list:
+
+    def get_recommendations(self, user_id):
         recomendaciones = recomendar_animes(user_id)
         return recomendaciones
